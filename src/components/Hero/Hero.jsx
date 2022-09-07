6e5bc51388e623c935d4ca51f912574520817e96
Jairo Jair Toro Novellis <108035450+jtnovellis@users.noreply.github.com>
2022-09-06 19:35:13 -0500

    Background Hero Mobile Done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import background from '../../images/homepage/bg-girl.png';
+import background from '../../images/homepage/bg-girl-small.png';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
@@ -12,6 +12,7 @@ function Hero() {
           <div className='background-img'>
             <img
               src={background}
+              className='bg-img'
               alt='Disfruta de viajar por Colombia y el mundo con LATAM Airlines'
             />
           </div>
@@ -19,7 +20,7 @@ function Hero() {
             <h1>
               Disfruta de viajar por Colombia y el mundo con LATAM Airlines
             </h1>
-            <Link to='/Destinos'>
+            <Link to='/Destinos' className='Destinos'>
               <span>Ver destinos</span>
               <FontAwesomeIcon
                 icon={faArrowUpRightFromSquare}
