97e6263902d1537176ba3ea4a3b1c28c7b0bdab7
Jairo Jair Toro Novellis <108035450+jtnovellis@users.noreply.github.com>
2022-09-06 19:04:15 -0500

    New dependencies

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header/index';
+import Hero from '../components/Hero/Hero';
+
+function Home() {
+  return (
+    <div className='container'>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+export default Home;
