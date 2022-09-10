d965f3865676038a8f47d6ef49f5603097cb5e7d
Jairo Jair Toro Novellis <108035450+jtnovellis@users.noreply.github.com>
2022-09-10 18:24:08 -0500

    Add Navbar Details

--- src/components/Header/Navmenu.jsx
+++ src/components/Header/Navmenu.jsx
@@ -3,26 +3,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import DropdownMenu from './DropdownMenu';
 
-function Navmenu({ content }) {
+function Navmenu() {
   const [drop, setDrop] = useState(false);
+  const [dropTwo, setDropTwo] = useState(false);
 
-  const handleClik = () => {
+  const handleOyFClik = () => {
     if (drop) {
       setDrop(false);
     } else {
       setDrop(true);
+      setDropTwo(false);
+    }
+  };
+  const handleMVClik = () => {
+    if (dropTwo) {
+      setDropTwo(false);
+    } else {
+      setDropTwo(true);
+      setDrop(false);
     }
   };
   return (
     <>
       <div className='ofertas-destino'>
-        <button type='button' name='ofertas-destino' onClick={handleClik}>
-          <span>{content}</span>
-          <FontAwesomeIcon icon={faAngleDown} className='faAngleDown' />
+        <button type='button' name='ofertas-destino' onClick={handleOyFClik}>
+          <span>Ofertas y destinos</span>
+          <FontAwesomeIcon
+            icon={faAngleDown}
+            className={!drop ? 'faAngleDown' : 'faAngleDownR'}
+          />
+        </button>
+      </div>
+      <div className='ofertas-destino'>
+        <button type='button' name='ofertas-destino' onClick={handleMVClik}>
+          <span>Mis viajes</span>
+          <FontAwesomeIcon
+            icon={faAngleDown}
+            className={!dropTwo ? 'faAngleDown' : 'faAngleDownR'}
+          />
         </button>
       </div>
       <DropdownMenu
-        className={drop ? 'dropdown-menu' : 'dropdown-menu-disable'}
+        className={drop ? 'dropdown-menu-OyD' : 'dropdown-menu-disable'}
+        classforStyle={dropTwo ? 'dropdown-menu-MV' : 'dropdown-menu-disable'}
       />
     </>
   );
