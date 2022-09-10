2ab5e5b93ddb3baa790359eb9fbd9dcb4bbf8d32
Noah <zap.noah@gmail.com>
2022-09-09 21:39:39 -0500

    Add the flight offers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/index';
 import Hero from '../components/Hero/Hero';
 
+
 function Home() {
   return (
     <div className='container'>
