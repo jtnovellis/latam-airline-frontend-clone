c8e6b873f1068aeb2cf921f67ddd1824481e3662
Noah <zap.noah@gmail.com>
2022-09-09 21:39:39 -0500

    Add the flight offers

---
+++ src/components/Mainhome/Flightdeals.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
+function Flightdeals({citySelected="Bogotá"}){
+    return(
+        <div className="fly-offers-menu-container">
+            <div>
+              <span className="menu-label-offers">Ofertas de vuelo desde </span>
+              <button id="cityselector">
+                <h2 className="cityselected">
+                  <span className="cityselectedname">{citySelected}</span>
+                  <span className="dropdown-arrow-menu">
+                  <FontAwesomeIcon icon={faAngleDown} className='faAngleDown' />
+                  </span>
+                </h2>
+              </button>
+            </div>
+        </div>
+    )
+}
+
+export default Flightdeals;
