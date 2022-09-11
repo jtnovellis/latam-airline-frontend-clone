4a52b8567668cc5096cc01cf5287f87a939ac5bd
Noah <zap.noah@gmail.com>
2022-09-11 13:33:35 -0500

    add the flight deals

--- src/components/MainInfo/index.jsx
+++ src/components/MainInfo/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import CovidInfo from './CovidInfo';
-
+import Flightdeals from '../FlightDeals/Flightdeals';
 function MainInfo() {
   return (
     <main className='main-content'>
       <section className='section-main-content'>
         <CovidInfo />
         <hr />
+        <Flightdeals />
       </section>
     </main>
   );
