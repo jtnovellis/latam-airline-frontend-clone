46560b9c710f1e9aa16df4194bec35654c87b9c7
Jairo Jair Toro Novellis <108035450+jtnovellis@users.noreply.github.com>
2022-09-16 17:25:23 -0500

    Start routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Register from './pages/Register'
+import Register from './pages/Register';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Register />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
   );
 }
