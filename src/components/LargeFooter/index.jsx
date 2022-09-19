1e94dfada6ebafe143d3e7c1babba1eca4ca7f3e
noahzapata <zap.noah@gmail.com>
2022-09-19 08:14:35 -0500

    add big footer

--- src/components/LargeFooter/index.jsx
+++ src/components/LargeFooter/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-const index = () => {
+const LargeFooter = () => {
   return (
     <div className='big-footer-container'>
       <div className='bg-ftr-cntnr-margins'>
-        <div>
+        <div className='ftr-inside-cntnr'>
           <hr />
-          <div></div>
+          <div className='LATAM-self-information'>
+            <div className='Ftr-info-cntnr'>
+              <h5 className='Footer-titles'>LATAM Airlines</h5>
+              <div className=''></div>
+            </div>
+            <div className='Ftr-info-cntnr'>
+              <h5 className='Footer-titles'>Información Leegal</h5>
+            </div>
+          </div>
+          <div className='Related-portals'></div>
+          <div className='Contact-certifications-appstoreLink'></div>
         </div>
       </div>
     </div>
   );
 };
 
-export default index;
+export default LargeFooter;
