0e33f535d4c14131f9565a7eca4ebbd691cf6d06
noahzapata <zap.noah@gmail.com>
2022-09-19 08:14:35 -0500

    add big footer

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,119 +1,200 @@
 export const flightsArr = [
-    {
-        id: 1,
-        cityImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/CLO-deals.jpg.transform/md/image.jpg',
-        city: 'Cali',
-        // departDate: { day: 18, month: 09, year: 2022 },
-        departDate: "18/11/22",
-        returnDate: '',
-        tripClass: 'Economy',
-        price: 192950.00,
-        tags: [ 'Tasas incluidas', 'Vuelo directo' ],
-        from: "Bogotá"
-    },
-    {
-        id: 2,
-        cityImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/ADZ-deals.jpg.transform/md/image.jpg',
-        city: 'Isla San Andrés',
-        // departDate: { day: 18, month: 09, year: 2022 },
-        departDate: '18/11/22',
-        returnDate: '',
-        tripClass: 'Economy',
-        price: 278800.00,
-        tags: [ 'Tasas incluidas', 'Vuelo directo' ],
-        from: "Bogotá"
-    },
-    {
-        id: 3,
-        cityImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/estados-unidos/deals/MIA-deals.jpg.transform/md/image.jpg',
-        city: 'Miami',
-        // departDate: { day: 18, month: 09, year: 2022 },
-        departDate: '26/11/22',
-        returnDate: '06/12/22',
-        tripClass: 'Economy',
-        price: 1766510.00,
-        tags: [ 'Tasas incluidas', 'Vuelo directo' ],
-        from: "Medellín"
-    },
-    {
-        id: 4,
-        cityImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/MDE2-deals.jpg.transform/md/image.jpg',
-        city: 'Medellín',
-        // departDate: { day: 18, month: 09, year: 2022 },
-        departDate: '20/11/22',
-        returnDate: '',
-        tripClass: 'Economy',
-        price: 203240.00,
-        tags: [ 'Tasas incluidas', 'Vuelo directo' ],
-        from: "Bogotá"
-    },
-    {
-        id: 5,
-        cityImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/CTG2-deals.jpg.transform/md/image.jpg',
-        city: 'Cartagena de indias',
-        // departDate: { day: 18, month: 09, year: 2022 },
-        departDate: '18/11/22',
-        returnDate: '',
-        tripClass: 'Economy',
-        price: 284311.00,       
-        tags: [ 'Tasas incluidas', 'Vuelo directo' ],
-        from: "Medellín"
-    },
-    {
-        id: 6,
-        cityImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/smr-deals.jpg.transform/md/image.jpg',
-        city: 'Santa Marta',
-        // departDate: { day: 18, month: 09, year: 2022 },
-        departDate: '18/11/22',
-        returnDate: '',
-        tripClass: 'Economy',
-        price:  224240.00,
-        tags: [ 'Tasas incluidas', 'Vuelo directo' ],
-        from: "Medellín"
-    },
-]
+  {
+    id: 1,
+    cityImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/CLO-deals.jpg.transform/md/image.jpg',
+    city: 'Cali',
+    // departDate: { day: 18, month: 09, year: 2022 },
+    departDate: '18/11/22',
+    returnDate: '',
+    tripClass: 'Economy',
+    price: 192950.0,
+    tags: ['Tasas incluidas', 'Vuelo directo'],
+    from: 'Bogotá',
+  },
+  {
+    id: 2,
+    cityImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/ADZ-deals.jpg.transform/md/image.jpg',
+    city: 'Isla San Andrés',
+    // departDate: { day: 18, month: 09, year: 2022 },
+    departDate: '18/11/22',
+    returnDate: '',
+    tripClass: 'Economy',
+    price: 278800.0,
+    tags: ['Tasas incluidas', 'Vuelo directo'],
+    from: 'Bogotá',
+  },
+  {
+    id: 3,
+    cityImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/estados-unidos/deals/MIA-deals.jpg.transform/md/image.jpg',
+    city: 'Miami',
+    // departDate: { day: 18, month: 09, year: 2022 },
+    departDate: '26/11/22',
+    returnDate: '06/12/22',
+    tripClass: 'Economy',
+    price: 1766510.0,
+    tags: ['Tasas incluidas', 'Vuelo directo'],
+    from: 'Medellín',
+  },
+  {
+    id: 4,
+    cityImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/MDE2-deals.jpg.transform/md/image.jpg',
+    city: 'Medellín',
+    // departDate: { day: 18, month: 09, year: 2022 },
+    departDate: '20/11/22',
+    returnDate: '',
+    tripClass: 'Economy',
+    price: 203240.0,
+    tags: ['Tasas incluidas', 'Vuelo directo'],
+    from: 'Bogotá',
+  },
+  {
+    id: 5,
+    cityImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/CTG2-deals.jpg.transform/md/image.jpg',
+    city: 'Cartagena de indias',
+    // departDate: { day: 18, month: 09, year: 2022 },
+    departDate: '18/11/22',
+    returnDate: '',
+    tripClass: 'Economy',
+    price: 284311.0,
+    tags: ['Tasas incluidas', 'Vuelo directo'],
+    from: 'Medellín',
+  },
+  {
+    id: 6,
+    cityImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/destinos/colombia/deals/smr-deals.jpg.transform/md/image.jpg',
+    city: 'Santa Marta',
+    // departDate: { day: 18, month: 09, year: 2022 },
+    departDate: '18/11/22',
+    returnDate: '',
+    tripClass: 'Economy',
+    price: 224240.0,
+    tags: ['Tasas incluidas', 'Vuelo directo'],
+    from: 'Medellín',
+  },
+];
 
-export const citiesArr = [ "Bogotá", "Medellín" ]
-
-
+export const citiesArr = ['Bogotá', 'Medellín'];
 
 export const imgPromoSectionArr = [
-    {
-        id:1,
-        smallImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/mobile/CO-FESTIVAL-12SEP-ZDC-MOBILE-768x286.jpg',
-        bigImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/desk/CO-FESTIVAL-12SEP-ZDC-DESKTOP-1024x308.jpg',
-        paragraph: "Viaja a Bogotá y disfruta del 'Festival Cordillera' este 24 y 25 de septiembre junto con LATAM "
-    },
-    {
-        id:2,
-        smallImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/mobile/CO-VDM-12SEP-ZDC-MOBILE-768x286.png',
-        bigImg: 'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/desk/CO-VDM-12SEP-ZDC-DESKTOP-1024x308.png',
-        paragraph: '¡Vive el amor! 50% Off en Millas LATAM Pass'
-    },
-]
+  {
+    id: 1,
+    smallImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/mobile/CO-FESTIVAL-12SEP-ZDC-MOBILE-768x286.jpg',
+    bigImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/desk/CO-FESTIVAL-12SEP-ZDC-DESKTOP-1024x308.jpg',
+    paragraph:
+      "Viaja a Bogotá y disfruta del 'Festival Cordillera' este 24 y 25 de septiembre junto con LATAM ",
+  },
+  {
+    id: 2,
+    smallImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/mobile/CO-VDM-12SEP-ZDC-MOBILE-768x286.png',
+    bigImg:
+      'https://www.latamairlines.com/content/dam/latamxp/sites/promociones/zona-de-campa%C3%B1as/andean/desk/CO-VDM-12SEP-ZDC-DESKTOP-1024x308.png',
+    paragraph: '¡Vive el amor! 50% Off en Millas LATAM Pass',
+  },
+];
 
+export const adviceArr = [
+  {
+    id: 1,
+    imgUrl:
+      'https://s.latamairlines.com/images/home/value_proposition_slider1.jpg',
+    title: 'Al preparar tu viaje',
+    description: 'Lo que necesitas saber con anticipación.',
+    other: 'Conoce más',
+  },
+  {
+    id: 2,
+    imgUrl:
+      'https://s.latamairlines.com/images/home/value_proposition_slider2.jpg',
+    title: 'En el aeropuerto',
+    description: 'Hacemos fácil el día de tu viaje.',
+    other: 'Conoce más',
+  },
+  {
+    id: 3,
+    imgUrl:
+      'https://s.latamairlines.com/images/home/value_proposition_slider3.jpg',
+    title: 'Durante el vuelo',
+    description: 'Relájate y disfruta de nuestro servicio a bordo.',
+    other: 'Conoce más',
+  },
+];
 
-export const adviceArr= [
-    {
-        id: 1,
-        imgUrl:'https://s.latamairlines.com/images/home/value_proposition_slider1.jpg',
-        title: 'Al preparar tu viaje',
-        description:'Lo que necesitas saber con anticipación.',
-        other:'Conoce más'
-    },
-    {
-        id: 2,
-        imgUrl:'https://s.latamairlines.com/images/home/value_proposition_slider2.jpg',
-        title: 'En el aeropuerto',
-        description:'Hacemos fácil el día de tu viaje.',
-        other:'Conoce más'
-    },
-    {
-        id: 3,
-        imgUrl:'https://s.latamairlines.com/images/home/value_proposition_slider3.jpg',
-        title: 'Durante el vuelo',
-        description:'Relájate y disfruta de nuestro servicio a bordo.',
-        other:'Conoce más'
-    },
-   
-]
+export const footerLinksAboutLATAM = [
+  {
+    id: `LargeFooterLatam-${this.description}`,
+    urlRedirec: '/',
+    description: 'Inicio',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Acerca de LATAM',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Experiencia LATAM',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Prepara tu viaje',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Mis viajes',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Estado de vuelo',
+  },
+  {
+    id: `LargeFooterLatam-${this.description}`,
+    urlRedirec: '/',
+    description: 'Check-in',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split}`,
+    urlRedirec: '/',
+    description: 'Destinos',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/register',
+    description: 'Crea tu cuenta',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Centro de ayuda',
+  },
+  {
+    id: `LargeFooterLatam-${this.description.split(' ').join('')}`,
+    urlRedirec: '/',
+    description: 'Sala de prensa',
+  },
+  {
+    id: `LargeFooterLatam-${this.description}`,
+    urlRedirec: '/',
+    description: 'Sostenibilidad',
+  },
+];
+
+export const footerLinksLegalInfo = [
+  {
+    id: `LargeFooterLegal-${this.description}`,
+    urlRedirec: '/',
+    description: 'Sostenibilidad',
+  },
+];
