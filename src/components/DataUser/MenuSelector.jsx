e5a451380d2ae7aa54edb1f504ad59be50d74e49
noahzapata <zap.noah@gmail.com>
2022-09-19 22:41:48 -0500

    add all

---
+++ src/components/DataUser/MenuSelector.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MenuSelector = props => {
+  const { path, children, current, setCurrent } = props;
+
+  function handleClick(e) {
+    const value = e.target.value;
+    if (value === '/my-account') {
+      setCurrent({
+        ...current,
+        '/my-account': !current[value],
+        'my-trips': false,
+        'pay-methods': false,
+      });
+    }
+    if (value === 'my-trips') {
+      setCurrent({
+        ...current,
+        '/my-account': false,
+        'my-trips': !current[value],
+        'pay-methods': false,
+      });
+    }
+    console.log(value);
+    if (value === 'pay-methods') {
+      setCurrent({
+        ...current,
+        '/my-account': false,
+        'my-trips': false,
+        'pay-methods': !current[value],
+      });
+    }
+  }
+
+  return (
+    <div className={`menuSelector ${current[path] ? 'selected' : null}`}>
+      <Link to={path} className='menuSelector__links'>
+        <button
+          className='menuSelector__links--buttons'
+          value={path}
+          onClick={e => handleClick(e)}>
+          <span>{children}</span>
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default MenuSelector;
--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import MenuSelector from '../components/DataUser/MenuSelector';
+import Header from '../components/Header';
+import Footer from '../components/Footer-Register';
+
+const menuOptions = [
+  {
+    slug: '/my-account',
+    content: 'Mi cuenta',
+  },
+  {
+    slug: 'my-trips',
+    content: 'Mis viajes',
+  },
+  {
+    slug: 'pay-methods',
+    content: 'Metodos de pago',
+  },
+];
 
 const MyAccount = () => {
+  const [selected, setSelected] = useState({
+    '/my-account': false,
+    'my-trips': false,
+    'pay-methods': false,
+  });
+
   return (
-    <div>
-      <h1>My Account</h1>
-      <Link to='my-trips'>mis viajes</Link>
-      <Link to='pay-methods'>metodos de pago</Link>
-      <Outlet />
-    </div>
+    <>
+      <Header />
+      <div className='myAccount'>
+        <div className='myAccount__infoContainer'>
+          <div className='myAccount__infoContainer-title'>
+            <h2>Mi Cuenta</h2>
+          </div>
+          <div className='myAccount__infoContainer-menu'>
+            {menuOptions.map(link => {
+              const { slug, content } = link;
+              return (
+                <>
+                  <hr />
+                  <MenuSelector
+                    key={content}
+                    path={slug}
+                    setCurrent={setSelected}
+                    current={selected}>
+                    {content}
+                  </MenuSelector>
+                </>
+              );
+            })}
+            <hr />
+          </div>
+        </div>
+        <div className='myAccount__bodyContainer'>
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
