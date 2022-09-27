dee3a4350403de8173fd8e0ec786d3971f269049
Omar Vega <omarvega598@gmail.com>
2022-09-27 14:03:41 -0500

    add structure
    Added header and footer

--- src/pages/Flights.jsx
+++ src/pages/Flights.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderRegister from '../components/Header-Register/HeaderRegister';
+import FlightContainer from '../components/BookingFlightSelector/FlightContainer';
 const Flights = () => {
   return (
     <>
       <HeaderRegister />
       <section></section>
+      <FlightContainer />
     </>
   );
 };
