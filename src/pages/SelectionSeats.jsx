a2c2714b2bf44e0b5a892c1daabb4bd7ee9f4b5b
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-09-27 09:46:23 -0500

    🛠  setup workspace

---
+++ src/pages/SelectionSeats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from 'components/Header-Register/HeaderRegister';
+
+const SelectionSeats = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Seleccion de asientos</h1>
+    </div>
+  );
+};
+
+export default SelectionSeats;
