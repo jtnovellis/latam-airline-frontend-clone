28eebc83e75ab5470850252d83cd1548f33d7f81
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-09-28 00:00:46 -0500

    Add airplane ✈️

--- src/components/SelectionSeats/Airplane.jsx
+++ src/components/SelectionSeats/Airplane.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import SeatsRow from './SeatsRow';
+import { firstDiv, secondDiv, thirthDiv } from '../../Airbus';
+import Emergency from 'components/SelectionSeats/Emergency';
+
+const headerSeats = ['A', 'B', 'C', '', 'D', 'E', 'F'];
 
 const Airplane = () => {
+  const airplane = '213';
   return (
     <div className='airplane'>
-      <h1>Airplane</h1>
+      <div className='airplane__body'>
+        <p>Air Bus {airplane}</p>
+        <div className='airplane__body--seats'>
+          <div className='seats--header'>
+            {headerSeats.map(item => (
+              <div key={item} className='seats--header-item'>
+                <span>{item}</span>
+              </div>
+            ))}
+          </div>
+          <div className='seats-firstDiv'>
+            {firstDiv.map((rows, i) => (
+              <SeatsRow key={i} rows={rows} />
+            ))}
+          </div>
+          <Emergency />
+          <div className='seats-secondDiv'>
+            {secondDiv.map((rows, i) => (
+              <SeatsRow key={i} rows={rows} />
+            ))}
+          </div>
+          <Emergency />
+          <div className='seats-secondDiv'>
+            {thirthDiv.map((rows, i) => (
+              <SeatsRow key={i} rows={rows} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
