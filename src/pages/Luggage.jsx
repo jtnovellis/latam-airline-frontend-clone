8650536f735ac223317ebd827e4783f9ba3ef789
Omar Vega <omarvega598@gmail.com>
2022-09-30 13:28:13 -0500

    add estructure

--- src/pages/Luggage.jsx
+++ src/pages/Luggage.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import LuggageCard from '../components/Luggage/LuggageCard';
+import HeaderRegister from '../components/Header-Register/HeaderRegister';
+import FooterRegister from '../components/Footer-Register/index';
+import { useState } from 'react';
 const Luggage = () => {
+  const [selected, setSelected] = useState(true);
+
   return (
     <>
-      <LuggageCard types='departure' />
-      <LuggageCard types='arrival' />
+      <HeaderRegister />
+      <div className='Luggage_wrapper'>
+        <h1>Compra tu equipaje ahora y paga menos en el aeropuerto</h1>
+        <div className='Luggage__selector'>
+          <div className='Luggagecard__body-departure-arrival'>
+            <div
+              style={selected ? { borderBottom: '2px solid red' } : {}}
+              onClick={() => (!selected ? setSelected(true) : <></>)}
+              className='Luggagecard__body-departure'>
+              <button>Ida</button>
+            </div>
+            <div
+              style={!selected ? { borderBottom: '2px solid red' } : {}}
+              onClick={() => (selected ? setSelected(false) : <></>)}
+              className='Luggagecard__body-arrival'>
+              <button>Vuelta</button>
+            </div>
+          </div>
+          {selected ? (
+            <LuggageCard types='departure' />
+          ) : (
+            <LuggageCard types='arrival' />
+          )}
+        </div>
+      </div>
+      <FooterRegister />
     </>
   );
 };
