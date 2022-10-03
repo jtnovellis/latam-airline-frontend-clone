5548edb14368b5089e995582e013782821c809fe
Daniel Moreno <daniel_9127@hotmail.com>
2022-10-02 19:27:50 -0500

    update h1 status flight

--- src/components/StatusFlight/StatusFlight.jsx
+++ src/components/StatusFlight/StatusFlight.jsx
@@ -5,7 +5,7 @@ const StatusFlight = () => {
   return (
     <div>
       <div className='statusf'>
-        <h1>resumen de tu viaje</h1>
+        <h1>Resumen de tu viaje</h1>
         <img src={image} alt='' />
         <h2>Aún no has seleccionado un vuelo</h2>
         <p>Tus vuelos aparecerán aquí una ves que los selecciones.</p>
