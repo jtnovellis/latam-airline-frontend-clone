e063495b03953dfa6f34430576b9953e5f6e2209
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-10-26 10:05:14 -0500

    work in payment

--- src/pages/PaymentResponse.jsx
+++ src/pages/PaymentResponse.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from 'components/Header-Register/HeaderRegister';
 import Footer from '../components/Footer-Register';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import { useState } from 'react';
 import Spinner from 'components/Spinner';
+import { useQuery } from '@tanstack/react-query';
 
 const PaymentResponse = () => {
-  const [paymentData, setPaymentData] = useState(null);
+  /* const [paymentData, setPaymentData] = useState(null); */
+  const { isLoading, error, data } = useQuery(['paymentData'], () => {
+    axios
+      .get(`https://secure.epayco.co/validation/v1/reference/${ePaycoRef}`)
+      .then(res => res.data);
+  });
   const navigate = useNavigate();
   // eslint-disable-next-line no-unused-vars
   const [queries, _] = useSearchParams();
@@ -17,7 +22,7 @@ const PaymentResponse = () => {
     navigate('/');
   };
 
-  useEffect(() => {
+  /* useEffect(() => {
     const fetchingData = async () => {
       try {
         const { data } = await axios.get(
@@ -29,26 +34,24 @@ const PaymentResponse = () => {
       }
     };
     fetchingData();
-  }, []);
+  }, []); */
+
+  if (isLoading) return <Spinner />;
+  if (error) return console.error(' algo paso compa revise');
   return (
     <>
       <Header />
-      {paymentData.success ? (
-        <Spinner />
-      ) : (
-        <div className='paymentResponse'>
-          <div className='paymentResponse__card'>
-            <h1>Datos de compra</h1>
-            <h2>
-              {paymentData.success ? 'Compra exitosa' : 'Compra no exitosa'}
-            </h2>
-            <p>Nombre: {paymentData.data.x_customer_name}</p>
-            <p>Id de compra: {paymentData.data.x_id_factura}</p>
-            <p>Precio total: {paymentData.data.x_amount}</p>
-            <button onClick={handleClick}>Volver al inicio</button>
-          </div>
+      <div className='paymentResponse'>
+        <div className='paymentResponse__card'>
+          <h1>Datos de compra</h1>
+          <h2>{data.success ? 'Compra exitosa' : 'Compra no exitosa'}</h2>
+          <p>Nombre: {data.data.x_customer_name}</p>
+          <p>Id de compra: {data.data.x_id_factura}</p>
+          <p>Precio total: {data.data.x_amount}</p>
+          <button onClick={handleClick}>Volver al inicio</button>
         </div>
-      )}
+      </div>
+
       <Footer />
     </>
   );
