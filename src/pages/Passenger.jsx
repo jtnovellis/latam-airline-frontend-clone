2607a1a1fb8294d89274c0aab6145b65a1e07b0b
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-10-26 09:16:18 -0500

    solving payment

--- src/pages/Passenger.jsx
+++ src/pages/Passenger.jsx
@@ -63,7 +63,7 @@ const Passenger = () => {
     extra2: 'extra2',
     extra3: 'extra3',
     // eslint-disable-next-line no-undef
-    response: 'http://secure2.payco.co/prueba_curl.php',
+    response: `https://latamairlines.vercel.app/payment-response`,
     name_billing: `${name || ''}`,
     address_billing: '',
     type_doc_billing: `${documentType || ''}`,
